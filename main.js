aede20bcf838f8c1d2d2b2773e3dcd08e75c8dad
Joseph Lander <josephclander@gmail.com>
2019-07-31 18:53:58 -0400

    scroll  up navbar js added

--- main.js
+++ main.js
@@ -3,6 +3,18 @@ function classToggle() {
   nav.classList.toggle("mobile__toggle");
 }
 
+var prevScrollpos = window.pageYOffset;
+function scrollUp() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("nav").style.top = "0";
+  } else {
+    document.getElementById("nav").style.top = "-66px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
 document.addEventListener("DOMContentLoaded", function(event) {
   document.getElementById("mobile").addEventListener("click", classToggle);
+  window.addEventListener("scroll", scrollUp);
 });
